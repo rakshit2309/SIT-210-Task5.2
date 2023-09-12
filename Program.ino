03b9389dd15b39e3c3a4c92d0d51c2c282a1ebb3
rakshit2309 <133203471+rakshit2309@users.noreply.github.com>
2023-09-12 15:29:33 +0530

    Program.ino

--- Program.ino
+++ Program.ino
@@ -1,66 +1,58 @@
-from Tkinter import *
-import tkFont
+import tkinter as tk
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(14,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
+GPIO.setup(16,GPIO.OUT)
 
 GPIO.setup(14,GPIO.LOW)
 GPIO.setup(15,GPIO.LOW)
-GPIO.setup(18,GPIO.LOW)
+GPIO.setup(16,GPIO.LOW)
 
-wink = Tk()
+win =tk.Tk()
+win.title("Rakshit GUI ")
+win.geometry('800x800')
 
-myFont =tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
+def toggle_red():
+    if GPIO.input(14):
+        GPIO.output(14, GPIO.LOW)
+        red_button.config(text="Red: OFF")
+    else:
+        GPIO.output(14, GPIO.HIGH)
+        red_button.config(text="Red: ON")
+        
+def toggle_green():
+    if GPIO.input(15):
+        GPIO.output(15, GPIO.LOW)
+        green_button.config(text="Green: OFF")
+    else:
+        GPIO.output(15, GPIO.HIGH)
+        green_button.config(text="Green: ON")
+        
+def toggle_blue():
+    if GPIO.input(16):
+        GPIO.output(16, GPIO.LOW)
+        blue_button.config(text="Blue: OFF")
+    else:
+        GPIO.output(18, GPIO.HIGH)
+        blue_button.config(text="Blue: ON")
+   
+red_button = tk.Radiobutton(win, text="Red: OFF", command=toggle_red)
+red_button.pack(pady=10)
+green_button = tk.Radiobutton(win, text="Green: OFF", command=toggle_green)
+green_button.pack(pady=10)
+blue_button = tk.Radiobutton(win, text="Blue: OFF", command=toggle_blue)
+blue_button.pack(pady=10)
 
-def red():
-	print("RED IS TURNED ON")
-	if GPIO.input(14) :
-		GPIO.output(14,GPIO.LOW)
-		ledButton["text"] = "RED ON"
-	
-	else :
-		GPIO.output(14,GPIO.HIGH)
-				ledButton["text"] = "RED OFF"
-def green():
-	print("RED IS TURNED ON")
-	if GPIO.input(15) :
-		GPIO.output(15,GPIO.LOW)
-		ledButton["text"] = "GREEN ON"
-	
-	else :
-		GPIO.output(14,GPIO.HIGH)
-				ledButton["text"] = "GREEN OFF"
-def blue():
-	print("BLUE IS TURNED ON")
-	if GPIO.input(14) :
-		GPIO.output(14,GPIO.LOW)
-		ledButton["text"] = "BLUE ON"
-	
-	else :
-		GPIO.output(14,GPIO.HIGH)
-				ledButton["text"] = "BLUE OFF"
-				
-				
-def exitprogram():
-	print("EXIT BUTTON PRESSED")
-		GPIO.cleanup()
-	win.quit()
+def exit_program():
+    GPIO.cleanup()
+    win.quit()
 
-win.title("GUI by Rahul")
-win.geometry('800x480')
 
-exitbutton = Button(win,text = "Exit", font =myFont, command = exitProgram, height = 2, width = 6)
-exitbutton.pack(side = BOTTOM)
+exit_button = tk.Button(win, text="Exit", command=exit_program)
+exit_button.pack(pady=10)
 
-redledButton = Button (win,text = "RED ON", font = myFont, command = red,height =2,width =8)
-redledButton.pack()
-greenledButton = Button (win,text = "GREEN ON", font = myFont, command = green,height =2,width =8)
-greenledButton.pack()
-blueledButton = Button (win,text = "BLUE ON", font = myFont, command = blue,height =2,width =8)
-blueledButton.pack()
 
-mainloop()
+win.mainloop()
